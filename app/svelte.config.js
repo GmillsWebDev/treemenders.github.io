9677d337f8de324b1396aba728dbeea3381b2a4f
Gary Mills <gmillswebdev@gmail.com>
2025-08-16 13:35:02 +0100

    adjuested again or github

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -3,8 +3,6 @@ import adapter from '@sveltejs/adapter-static';
 export default {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
@@ -12,7 +10,7 @@ export default {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: '/treemenders.github.io'
 		}
 	}
 };
